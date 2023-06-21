f7707c73a6d4dfd3938245482a037dd81ef04aa6
chehinedh <chehine.dhemaied@esprit.tn>
2023-06-21 16:57:02 +0100

    ✅ SignIn

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def home(request):
@@ -21,7 +22,7 @@ def signup(request):
         myuser = User.objects.create_user(username, email, password)
         myuser.first_name = firstname
         myuser.last_name = lastname
-        
+
         myuser.save()
         messages.success(request, 'Your Account has been successfully created.')
         return redirect('signin')
@@ -29,7 +30,23 @@ def signup(request):
     return render(request, 'authentication/signup.html')
 
 def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            firstname = user.first_name
+            return render(request, "authentication/index.html", {'firstname': firstname})
+        else:
+            messages.error(request, 'Invalid Credentials, Please try again')
+            return redirect('home')
+
     return render(request, 'authentication/signin.html')
+
+   
 
 def signout(request):
     pass

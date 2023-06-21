2dfbd8539d2bb627e98e93c08e1e22f2bd5410f1
chehinedh <chehine.dhemaied@esprit.tn>
2023-06-21 17:55:13 +0100

    🚀 send email verification

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from user import settings
+from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
@@ -18,6 +20,27 @@ def signup(request):
         email = request.POST['email']
         password = request.POST['password']
         pass2 = request.POST['pass2']
+
+        if User.objects.filter(username=username):
+            messages.error(request, "Username aldready exists!")
+            return redirect('home')
+        
+        if User.objects.filter(email=email):
+            messages.error(request, "Email aldready exists!")    
+            return redirect('home')
+        
+        if len(username) > 10:
+            messages.error(request, "Username must be under 10 characters!")
+            return redirect('home')
+        
+        if password != pass2:
+            messages.error(request, "Passwords do not match!")
+            return redirect('home')
+        
+        if not username.isalnum():
+            messages.error(request, "Username should only contain letters and numbers!")
+            return redirect('home')
+
         # Create the user
         myuser = User.objects.create_user(username, email, password)
         myuser.first_name = firstname
@@ -25,6 +48,16 @@ def signup(request):
 
         myuser.save()
         messages.success(request, 'Your Account has been successfully created.')
+
+        # welcome email
+
+        subject = 'Welcome to Django Login!'
+        message = "Hello" + myuser.first_name + "!! \n" + "Welcome to Django \n Thank you for visiting our website \n We have also sent you a conformation email, please confirm your email address in order to activate your account" 
+        from_email= settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+
+
         return redirect('signin')
 
     return render(request, 'authentication/signup.html')
